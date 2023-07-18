8c17093d8d3ee260ce2ead974eff1514db9b9846
godoimatheus <matheus.godoi@live.com>
2023-07-18 09:21:48 -0300

    feat: adiciona rota de autenticacao

--- projeto_vagas/projeto_vagas/urls.py
+++ projeto_vagas/projeto_vagas/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from app_vagas import views
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path("", views.login, name="login"),
-
-    path("cadastro/", views.cadastro, name="cadastro")
+    path("auth/", include("app_vagas.urls"))
 ]
