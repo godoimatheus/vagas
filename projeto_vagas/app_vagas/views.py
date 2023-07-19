2b74716168e2c5d9af99671cf24d1d1e051b0ad4
godoimatheus <matheus.godoi@live.com>
2023-07-19 12:43:27 -0300

    refactor: remove else desnecessarios antes de return

--- projeto_vagas/app_vagas/views.py
+++ projeto_vagas/app_vagas/views.py
@@ -4,47 +4,47 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 
 def home(request):
     return render(request, "home.html")
 
+
 def cadastro(request):
     if request.method == "GET":
         return render(request, "cadastro.html")
-    else:
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
 
-        user = User.objects.filter(username=username).first()
+    username = request.POST.get("username")
+    email = request.POST.get("email")
+    senha = request.POST.get("senha")
 
-        if user:
-            return HttpResponse("Já cadastrado")
+    user = User.objects.filter(username=username).first()
 
-        user = User.objects.create_user(username=username, email=email, password=senha)
-        user.save()
+    if user:
+        return HttpResponse("Já cadastrado")
 
-        return HttpResponse("Usuário cadastrado com sucesso")
+    user = User.objects.create_user(username=username, email=email, password=senha)
+    user.save()
+
+    return HttpResponse("Usuário cadastrado com sucesso")
 
 
 def login(request):
     if request.method == "GET":
         return render(request, "login.html")
-    else:
-        username = request.POST.get('username')
-        senha = request.POST.get('senha')
+    username = request.POST.get("username")
+    senha = request.POST.get("senha")
 
-        user = authenticate(username=username, password=senha)
+    user = authenticate(username=username, password=senha)
 
-        if user:
-            login_django(request, user)
-            return HttpResponse("autenticado")
-        else:
-            return HttpResponse("Email ou senha inválidos")
+    if user:
+        login_django(request, user)
+        return HttpResponse("autenticado")
+    return HttpResponse("Email ou senha inválidos")
 
 
 @login_required(login_url="/auth/login/")
-def plataforma(request):
+def plataforma():
     return HttpResponse("Plataforma")
