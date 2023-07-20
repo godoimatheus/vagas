7fbbb7d46a58fc4427b7cc8e903c500f28e5e4d8
godoimatheus <matheus.godoi@live.com>
2023-07-19 21:56:56 -0300

    feat: adiciona rotas de exibir, criar e salvar vagas

--- projeto_vagas/empresas/urls.py
+++ projeto_vagas/empresas/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("cadastro/", views.cadastro_empresas, name="cadastro_empresas"),
-    path("login/", views.login_empresas, name="login_empresas")
+    path("login/", views.login_empresas, name="login_empresas"),
+    path("vagas/", views.vagas, name="vagas"),
+    path("vagas/criar", views.criar_vaga, name="criar_vaga"),
+    path("vagas/salvar", views.salvar_vaga, name="salvar_vaga"),
 ]
