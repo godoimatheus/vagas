a85bdfe6f957c2cc11cc39f2582f863e6b2b1f87
godoimatheus <matheus.godoi@live.com>
2023-07-21 15:41:01 -0300

    refactor: remove urls nao usadas

--- projeto_vagas/app_vagas/urls.py
+++ projeto_vagas/app_vagas/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("cadastro/", views.cadastro, name="cadastro"),
-    path("login/", views.login, name="login"),
-    path("plataforma/", views.plataforma, name="plataforma"),
     path("vagas/", views.vagas_home, name="vagas_home"),
     path("vagas/<int:vaga_id>", views.vagas_detalhes, name="vagas_detalhes"),
     path("vagas/<int:vaga_id>/candidatar", views.candidatar_vaga, name="candidatar_vaga"),
