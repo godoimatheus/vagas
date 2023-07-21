64bfa5dab13e20dcf462c11af369561198960285
godoimatheus <matheus.godoi@live.com>
2023-07-21 12:05:27 -0300

    feat: adiciona formulario do candidato

---
+++ projeto_vagas/app_vagas/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Candidatura
+
+
+class CandidaturaForm(forms.ModelForm):
+    class Meta:
+        model = Candidatura
+        fields = ['pretensao_salario', 'experiencia', 'ultima_escolaridade']
+        labels = {
+            'pretensao_salario': 'Pretensão Salarial',
+            'experiencia': 'Experiência',
+            'ultima_escolaridade': 'Última Escolaridade',
+        }
