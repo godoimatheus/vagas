07d75ade769be7203341207ee8328acd36405dbd
godoimatheus <matheus.godoi@live.com>
2023-07-20 22:27:49 -0300

    feat: adiciona urls que permite o usuario se candidatar a vaga

--- projeto_vagas/app_vagas/urls.py
+++ projeto_vagas/app_vagas/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("cadastro/", views.cadastro, name="cadastro"),
     path("login/", views.login, name="login"),
     path("plataforma/", views.plataforma, name="plataforma"),
+    path("vagas/", views.vagas_home, name="vagas_home"),
+    path("vagas/<int:vaga_id>", views.vagas_detalhes, name="vagas_detalhes"),
+    path("vagas/<int:vaga_id>/candidatar", views.vagas_candidatar, name="vagas_candidatar"),
 ]
