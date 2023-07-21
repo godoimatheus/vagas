4b4515e61aeb1d6a9db6267f12bc27b0a7ed3eb0
godoimatheus <matheus.godoi@live.com>
2023-07-21 15:43:02 -0300

    refactor: remove funcao nao usadas

--- projeto_vagas/app_vagas/views.py
+++ projeto_vagas/app_vagas/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from empresas.models import Vagas
 from .models import Candidatura
 from .forms import CandidaturaForm
+
 # Create your views here.
 
 
@@ -14,55 +15,18 @@ def home(request):
     return render(request, "home.html")
 
 
-def cadastro(request):
-    if request.method == "GET":
-        return render(request, "cadastro.html")
-
-    username = request.POST.get("username")
-    email = request.POST.get("email")
-    senha = request.POST.get("senha")
-
-    user = User.objects.filter(username=username).first()
-
-    if user:
-        return HttpResponse("Já cadastrado")
-
-    user = User.objects.create_user(username=username, email=email, password=senha)
-    user.save()
-
-    return HttpResponse("Usuário cadastrado com sucesso")
-
-
-def login(request):
-    if request.method == "GET":
-        return render(request, "login.html")
-    username = request.POST.get("username")
-    senha = request.POST.get("senha")
-
-    user = authenticate(username=username, password=senha)
-
-    if user:
-        login_django(request, user)
-        return HttpResponse("autenticado")
-    return HttpResponse("Email ou senha inválidos")
-
-
-@login_required(login_url="/auth/login/")
-def plataforma():
-    return HttpResponse("Plataforma")
-
-
+@login_required(login_url="/")
 def vagas_home(request):
     vagas = Vagas.objects.all()
     return render(request, "vagas_home.html", {"vagas": vagas})
 
-
+@login_required(login_url="/")
 def vagas_detalhes(request, vaga_id):
     vaga = get_object_or_404(Vagas, pk=vaga_id)
     return render(request, "vagas_detalhes.html", {"vaga": vaga})
 
 
-@login_required
+@login_required(login_url="/")
 def candidatar_vaga(request, vaga_id):
     vaga = get_object_or_404(Vagas, pk=vaga_id)
     candidato = request.user
