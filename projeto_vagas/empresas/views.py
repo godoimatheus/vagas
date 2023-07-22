0acb2c8e3d1837cdb31fe89e951214ebe3dad519
godoimatheus <matheus.godoi@live.com>
2023-07-22 17:47:33 -0300

    refactor: adequa ao pylint

--- projeto_vagas/empresas/views.py
+++ projeto_vagas/empresas/views.py
@@ -1,51 +1,46 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http.response import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, logout
 from django.contrib.auth import login as login_django
 from django.contrib.auth.decorators import login_required
-from .models import Vagas
-from .forms import FormVagas
 from app_vagas.models import Candidatura
 from rolepermissions.roles import assign_role
 from rolepermissions.decorators import has_role_decorator
+from .models import Vagas
+from .forms import FormVagas
 
+
+# pylint: disable=E1101
 # Create your views here.
-
-
 def cadastro_empresas(request):
     if request.method == "GET":
         return render(request, "empresas/cadastro.html")
-    else:
-        username = request.POST.get("username")
-        email = request.POST.get("email")
-        senha = request.POST.get("senha")
+    username = request.POST.get("username")
+    email = request.POST.get("email")
+    senha = request.POST.get("senha")
 
-        user = User.objects.filter(username=username).first()
+    user = User.objects.filter(username=username).first()
 
-        if user:
-            return redirect("login_empresas")
-
-        user = User.objects.create_user(username=username, email=email, password=senha)
-        user.save()
-        assign_role(user, "empresa")
+    if user:
         return redirect("login_empresas")
+
+    user = User.objects.create_user(username=username, email=email, password=senha)
+    user.save()
+    assign_role(user, "empresa")
+    return redirect("login_empresas")
 
 
 def login_empresas(request):
     if request.method == "GET":
         return render(request, "empresas/login.html")
-    else:
-        username = request.POST.get("username")
-        senha = request.POST.get("senha")
+    username = request.POST.get("username")
+    senha = request.POST.get("senha")
+    user = authenticate(username=username, password=senha)
 
-        user = authenticate(username=username, password=senha)
-
-        if user:
-            login_django(request, user)
-            return redirect("vagas")
-        else:
-            return render(request, "empresas/login_erro.html")
+    if user:
+        login_django(request, user)
+        return redirect("vagas")
+    return render(request, "empresas/login_erro.html")
 
 
 def logout_view(request):
@@ -55,7 +50,7 @@ def logout_view(request):
 
 @login_required(login_url="/empresas/login")
 @has_role_decorator("empresa")
-def vagas(request):
+def todas_vagas(request):
     vagas = Vagas.objects.all()
     return render(request, "empresas/vagas.html", {"vagas": vagas})
 
