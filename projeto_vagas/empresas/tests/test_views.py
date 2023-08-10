dd0d93fb12b8c4353e3f248f7a968b71b1e47163
godoimatheus <matheus.godoi@live.com>
2023-08-10 11:19:49 -0300

    test: verifica redirecionamento apos cadastro

--- projeto_vagas/empresas/tests/test_views.py
+++ projeto_vagas/empresas/tests/test_views.py
@@ -10,3 +10,15 @@ class EmpresasViewTest(TestCase):
         response = self.client.get(reverse("cadastro_empresas"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "empresas/cadastro.html")
+
+    def test_redirecionamento_apos_cadastro(self):
+        response = self.client.post(
+            reverse("cadastro_empresas"),
+            {
+                "username": "empresa",
+                "email": "empresa@email.com",
+                "password": "senha123",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/empresas/cadastro/")
