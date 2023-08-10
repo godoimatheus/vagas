cfda9360a516892c58534d4913f6ec1f762ec050
godoimatheus <matheus.godoi@live.com>
2023-08-09 21:55:04 -0300

    test: verifica pagina de detalhes de vagas

--- projeto_vagas/candidatos/tests/test_views.py
+++ projeto_vagas/candidatos/tests/test_views.py
@@ -3,6 +3,9 @@ from django.test import Client, TestCase
 from django.urls import reverse
 from rolepermissions.roles import assign_role
 from rolepermissions.checkers import has_role
+from empresas.models import Vagas
+
+# pylint: disable=E1101
 
 
 class CandidatosViewsTest(TestCase):
@@ -15,6 +18,10 @@ class CandidatosViewsTest(TestCase):
             username=self.username, email=self.email, password=self.password
         )
         assign_role(self.candidato, "candidato")
+        self.empresa = User.objects.create_user(
+            username="empresa", email="empresa@email.com", password="senha123"
+        )
+        assign_role(self.empresa, "empresa")
 
     def test_status_pagina_cadastro_e_template(self):
         response = self.client.get(reverse("cadastro_candidatos"))
@@ -58,3 +65,15 @@ class CandidatosViewsTest(TestCase):
         response = self.client.get(reverse("vagas_home"))
         self.assertEqual(response.status_code, 302)
         self.assertFalse(has_role(self.candidato, "empresa"))
+
+    def test_pagina_de_detalhes_vagas(self):
+        vaga = Vagas.objects.create(
+            empresa=self.empresa.username,
+            titulo="Vaga de Teste",
+            salario="Até 1000",
+            escolaridade="Ensino fundamental",
+        )
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(reverse("vagas_detalhes", args=[vaga.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "candidatos/vagas_detalhes.html")
