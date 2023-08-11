80d3c9b5a82319517170f2433374c46b33ed564c
godoimatheus <matheus.godoi@live.com>
2023-08-11 09:56:34 -0300

    test: verifica status e template pagina home

---
+++ projeto_vagas/app_vagas/tests/test_views.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestViewsAppVagas(TestCase):
+    def test_status_e_template_home(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
