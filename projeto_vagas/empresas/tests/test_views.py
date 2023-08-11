d47a6e47bc407efe608a452affbcad457c2d400c
godoimatheus <matheus.godoi@live.com>
2023-08-11 16:40:14 -0300

    test: verifica criar editar excluir e detalhes das vagas

--- projeto_vagas/empresas/tests/test_views.py
+++ projeto_vagas/empresas/tests/test_views.py
@@ -3,6 +3,10 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from rolepermissions.roles import assign_role
 from rolepermissions.checkers import has_role
+from candidatos.models import Candidatura
+from ..models import Vagas
+
+# pylint: disable=E1101
 
 
 class EmpresasViewTest(TestCase):
@@ -68,3 +72,123 @@ class EmpresasViewTest(TestCase):
         response = self.client.get(reverse("vagas"))
         self.assertEqual(response.status_code, 302)
         self.assertFalse(has_role(self.empresa, "candidato"))
+
+    def test_pagina_de_criar_vagas(self):
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(reverse("criar_vaga"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "empresas/criar_vaga.html")
+
+    def test_form_criar_vagas_redirecionamento_e_se_esta_salvando(self):
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.post(
+            reverse("salvar_vaga"),
+            {
+                "titulo": "Vaga de teste",
+                "salario": "1",
+                "escolaridade": "1",
+            },
+        )
+        nova_vaga = Vagas.objects.get(titulo="Vaga de teste")
+        self.assertEqual(nova_vaga.empresa, self.username)
+        self.assertEqual(nova_vaga.salario, "Até 1000")
+        self.assertEqual(nova_vaga.escolaridade, "Ensino fundamental")
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("vagas"))
+
+    def test_pagina_de_detalhes_vagas(self):
+        vaga = Vagas.objects.create(
+            empresa=self.empresa.username,
+            titulo="Vaga de Teste",
+            salario="Até 1000",
+            escolaridade="Ensino fundamental",
+        )
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(reverse("detalhes_vaga", args=[vaga.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "empresas/detalhes_vaga.html")
+
+    def test_pagina_de_editar_vagas(self):
+        vaga = Vagas.objects.create(
+            empresa=self.empresa.username,
+            titulo="Vaga de teste",
+            salario="Até 1000",
+            escolaridade="Ensino fundamental",
+        )
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(reverse("editar_vaga", args=[vaga.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "empresas/editar_vaga.html")
+
+    def test_form_edicao_de_vagas_verifica_redirecionamento_e_salvamento(self):
+        self.client.login(username=self.username, password=self.password)
+        vaga = Vagas.objects.create(
+            empresa=self.empresa.username,
+            titulo="Vaga de teste",
+            salario="Até 1000",
+            escolaridade="Ensino fundamental",
+        )
+        response = self.client.post(
+            reverse("editar_vaga", args=[vaga.id]),
+            {"titulo": "Vaga de teste editada", "salario": "2", "escolaridade": "2"},
+        )
+        vaga_editada = Vagas.objects.get(titulo="Vaga de teste editada")
+        self.assertEqual(vaga_editada.empresa, self.username)
+        self.assertEqual(vaga_editada.titulo, "Vaga de teste editada")
+        self.assertEqual(vaga_editada.salario, "De 1000 a 2000")
+        self.assertEqual(vaga_editada.escolaridade, "Ensino médio")
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("detalhes_vaga", args=[vaga.id]))
+
+    def test_deletar_vagas(self):
+        self.client.login(username=self.username, password=self.password)
+        vaga = Vagas.objects.create(
+            empresa=self.username,
+            titulo="Vaga de teste",
+            salario="Até 1000",
+            escolaridade="Ensino fundamental",
+        )
+        response = self.client.post(reverse("deletar_vaga", args=[vaga.id]))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("vagas"))
+
+        try:
+            self.assertTrue(Vagas.objects.get(titulo="Vaga de teste"))
+        except Vagas.DoesNotExist:
+            pass
+
+    def test_pagina_de_detalhes_dos_candidatos(self):
+        self.client.login(username=self.username, password=self.password)
+        vaga = Vagas.objects.create(
+            empresa=self.username,
+            titulo="Vaga de teste",
+            salario="Até 1000",
+            escolaridade="Ensino fundamental",
+        )
+        candidato = User.objects.create_user(username="candidato", password="senha123")
+        candidatura = Candidatura.objects.create(
+            candidato=candidato,
+            vaga=vaga,
+            pretensao_salario="Até 1000",
+            experiencia="Até 1 ano",
+            ultima_escolaridade="Ensino fundamental",
+        )
+        response = self.client.get(reverse("candidatos_vaga", args=[vaga.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "empresas/candidatos_vaga.html")
+        self.assertIn(candidatura, response.context["candidaturas"])
+        candidatura.refresh_from_db()
+        self.assertEqual(candidatura.pontuacao, 2)
+
+    def test_vaga_sem_candidatos(self):
+        self.client.login(username=self.username, password=self.password)
+        vaga = Vagas.objects.create(
+            empresa=self.username,
+            titulo="Vaga de teste",
+            salario="Até 1000",
+            escolaridade="Ensino fundamental",
+        )
+        response = self.client.get(reverse("candidatos_vaga", args=[vaga.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "empresas/candidatos_vaga.html")
+        self.assertEqual(len(response.context["candidaturas"]), 0)
