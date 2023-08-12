96dc6da3c05b5a1929e371e31e65eba0a0c4df64
godoimatheus <matheus.godoi@live.com>
2023-08-11 21:49:31 -0300

    refactor: renomeia app_vagas para home

--- projeto_vagas/home/admin.py
+++ projeto_vagas/home/admin.py
@@ -1,3 +1,4 @@
+# pylint: disable=W0611
 from django.contrib import admin
 
 # Register your models here.
--- projeto_vagas/home/apps.py
+++ projeto_vagas/home/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppVagasConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "app_vagas"
+    name = "home"
